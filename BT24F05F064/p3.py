49483f27a615ea28c8a9c7040eb606f0ceea3796
shraddhaujgare6-commits <shraddhaujgare6@gmail.com>
2026-04-19 14:11:01 +0530

    Add variable and data type examples in p3.py

---
+++ BT24F05F064/p3.py
@@ -0,0 +1,29 @@
+practical 3 : variable and data type
+a = 10
+b = 5.5
+c = "100"
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+x = int(c)
+y = float(c)
+z = str(a)
+w = bool(a)
+
+print(x, type(x))
+print(y, type(y))
+print(z, type(z))
+print(w, type(w))
+
+p = 50
+print(p, type(p))
+
+p = "Dynamic typing"
+print(p, type(p))
+
+p = 9.8
+print(p, type(p))
